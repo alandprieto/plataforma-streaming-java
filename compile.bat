911011a34322bc488ae1281ced5500835198d9dc
Alan Prieto <alan.prieto@alu.ing.unlp.edu.ar>
2026-09-16 16:09:22 -0300

    fix: clean bin before compiling, prefer JAVA_HOME

--- compile.bat
+++ compile.bat
@@ -1,9 +1,11 @@
 @echo off
 setlocal
 set OUT=bin
-if not exist "%OUT%" mkdir "%OUT%"
+rd /s /q "%OUT%" 2>nul
+mkdir "%OUT%"
+if defined JAVA_HOME (set "JAVAC=%JAVA_HOME%\bin\javac.exe") else (set "JAVAC=javac")
 dir /s /b src\*.java > sources.txt
-javac -encoding UTF-8 -cp "lib/*;src" -d "%OUT%" @sources.txt
+"%JAVAC%" -encoding UTF-8 -cp "lib/*;src" -d "%OUT%" @sources.txt
 set RESULT=%ERRORLEVEL%
 if exist sources.txt del sources.txt
 if %RESULT% EQU 0 (
